1d044f7cf4660f88e5cfd5778fb3f6b5188050c0
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-05-28 11:04:49 -0400

    Adição do fastify e Zod no programa

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,27 @@
 import "dotenv/config";
 
 import Fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+
 const app = Fastify({
   logger: true,
 });
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.get("/", async function handler() {
   return { hello: "world" };
 });
 
 try {
-  await app.listen({ port: Number(process.env.PORT) });
+  await app.listen({
+    port: Number(process.env.PORT),
+  });
+
   console.log(`Server is running on port ${process.env.PORT}`);
 } catch (err) {
   app.log.error(err);

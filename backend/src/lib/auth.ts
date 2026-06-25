dec4a1f8696333680136b1fd247a085c22ee3e31
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-25 12:59:53 -0400

    Autenticação com o Google - Configurando o auth.ts para ler as variaveis GOOGLE_CLIENT_ID e GOOGLE_CLIENT_SECRET

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -7,9 +7,14 @@ import { prisma } from "./db.js";
 
 export const auth = betterAuth({
   trustedOrigins: ["http://localhost:3000"],
-  emailAndPassword: {
-    enabled: true,
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
+
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   plugins: [openAPI()],
 });
